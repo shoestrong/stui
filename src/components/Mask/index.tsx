343f2ae3e353201632da95f33cf6cd26368abe73
shoestrong <zhangqianchuang@tongniutianxia.com>
2022-03-15 15:03:51 +0800

    feat: add mask

---
+++ src/components/Mask/index.tsx
@@ -0,0 +1,19 @@
+import React, { FC } from 'react';
+import classNames from 'classnames';
+import './index.less';
+
+interface MaskProps {
+  className?: string;
+  style?: React.CSSProperties;
+  children?: React.ReactNode;
+  onClick?: React.MouseEventHandler<HTMLElement>;
+}
+
+const cssPrefix: string = 'st-mask';
+const Mask: FC<MaskProps> = ({ className, style, children, onClick }) => (
+  <div className={classNames(cssPrefix, className)} style={style} onClick={onClick}>
+    {children}
+  </div>
+);
+
+export default Mask;
